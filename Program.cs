c1d620e37fdf5dc2f4f296aa3c845a6d703ecdd2
Leohub11 <olive.ng1812@gmail.com>
2025-01-14 02:07:15 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a1_LeoNguyen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nI am excited about writing my first program.");
+            Console.WriteLine("I know \"Practice Makes Perfect\"");
+            Console.Write("\n\n*        "); Console.WriteLine("\t\t*        *");
+            Console.Write("*        "); Console.WriteLine("\t\t**       *");
+            Console.Write("*        "); Console.WriteLine("\t\t* *      *");
+            Console.Write("*        "); Console.WriteLine("\t\t*  *     *");
+            Console.Write("*        "); Console.WriteLine("\t\t*   *    *");
+            Console.Write("*        "); Console.WriteLine("\t\t*    *   *");
+            Console.Write("*        "); Console.WriteLine("\t\t*     *  *");
+            Console.Write("*        "); Console.WriteLine("\t\t*      * *");
+            Console.Write("*********"); Console.WriteLine("\t\t*       **");
+
+            Console.ReadLine();
+
+        }
+    }
+}
